db726c9176d7b7d70513f3647ec2eeaeba28fcfb
shin <suoxin117@gmail.com>
2013-06-07 11:51:22 +0800

    init Project

---
+++ PracCococs2dX/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  PracCococs2dXAppController.h
+//  PracCococs2dX
+//
+//  Created by shin on 13-6-7.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
